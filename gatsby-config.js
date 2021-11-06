c5201fe3024abe3ed5462de440cc59b4efdb1fa0
Mitsuki <93656084+S-WALKER-S@users.noreply.github.com>
2021-11-06 21:06:38 +0900

    Add files via upload

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,12 @@
  */
 
 module.exports = {
+  siteMetadata:{
+    title: "My First Gatsby Site",
+  },
   /* Your site config here */
-  plugins: [],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp"
+  ],
 }
